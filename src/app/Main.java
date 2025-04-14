30f2a63141df0efb73e5e5db697cc98123a3101b
PepsaBoy <Serebro.75@ukr.net>
2025-04-14 17:02:14 +0300

    Add the functionality of converting Fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,16 @@
 package app;
 
 public class Main {
+    private static final int Const=32;
+    private static final double Conv= (double) 5 /9;
     public static void main(String[] args) {
         System.out.println("Temperature unit converter");
+        double Fahrenheits=150;
+        double Celsius= convFahrenheitsToCelsius(Fahrenheits);
+        System.out.println("Result is "+Celsius);
+    }
+    private static double convFahrenheitsToCelsius(double Fahrenheits){
+        return (Fahrenheits-Const)*Conv;
+
     }
 }
